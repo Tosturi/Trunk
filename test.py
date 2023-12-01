ce2edcff30d81bec3f075cb7ab55e23efc6e6271
Tosturi <allexgodofwar@gmail.com>
2023-12-01 22:06:33 +0500

    e**x

--- test.py
+++ test.py
@@ -5,20 +5,17 @@ def factorial():
         fact = fact * stp
         stp += 1
         yield fact
-        # if stp > steps:
-        #     return
 
 
-def iks():
-    step = 1
-    while True:
-        step += 1
-        yield step
+def degree(n):
+    def factorization(m):
+        return m**n
+    return factorization
 
 
 ff = factorial()
 res = 1
-for _ in range(10):
-    f = (lambda x, y: (1**x/y))(iks().__next__(), factorial().__next__())
-    res += f
+for i in range(int(input())):
+    count = degree(i)
+    res += (lambda: count(1)/next(ff))()
     print(res)
