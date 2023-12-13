49c76b8c1c5b9fd1face893be9e6718567f4ae77
Tosturi <allexgodofwar@gmail.com>
2023-12-14 01:16:18 +0500

    upd

--- World_of_NavalCombat/main.py
+++ World_of_NavalCombat/main.py
@@ -3,7 +3,21 @@ from People import Human
 from Bots import Bot
 from Session import GameSession
 
+
+def player_input(player_number: int):
+    """В данной функции игрок выбирает кто будет играть"""
+    who_is_player = input("Who will play? (User/bot): ").lower()
+    if who_is_player == 'user':
+        user_name = input("Enter the user name: ")
+    else:
+        user_name = f"Bot {player_number}"
+
+    if who_is_player not in ['user', 'bot']:
+        return player_input(player_number)
+    else:
+        return who_is_player, user_name
+
+
 if __name__ == '__main__':
-    player1 = Human("Вася")
-    player2 = Bot()
-    game = GameSession(player1, player2)
+    player1 = player_input(1)
+    print(player1)
