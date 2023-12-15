ad0b86ae205652f122e5c95d37812938669d722c
Tosturi <allexgodofwar@gmail.com>
2023-12-15 21:52:56 +0500

    upd

--- World_of_NavalCombat/People.py
+++ World_of_NavalCombat/People.py
@@ -5,12 +5,9 @@ from enums import Direction
 class Human(CreatePlayer):
     """Создание игрока-человека"""
 
-    def __init__(self, name: str):
-        super().__init__()
-        self.name = name
-
     def deploy_fleet(self):
         """Расстановка кораблей игроком"""
+        row, column =
 
     def shoot(self) -> tuple[int, int]:
         """Спросить у игрока координаты выстрела"""
